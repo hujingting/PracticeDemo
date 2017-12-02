431626e46741b3a099150c80f4e0189ace0a25bc
hujingting <15601863591@163.com>
2017-12-02 17:00:17 +0800

    test

--- app/src/main/java/com/tutao/practicedemo/test.java
+++ app/src/main/java/com/tutao/practicedemo/test.java
@@ -1,11 +1,30 @@
 package com.tutao.practicedemo;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by jingting on 2017/11/30.
  */
 
-public class test {
-    public test() {
+public class test extends View{
 
+
+    public test(Context context) {
+        super(context);
+    }
+
+    public test(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public test(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public test(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 }
