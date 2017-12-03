4d2274e989bcd7234a3452deab40f4ae3a0d5613
hujingting <15601863591@163.com>
2017-12-03 16:15:50 +0800

    paint

--- app/src/main/java/com/tutao/practicedemo/test.java
+++ app/src/main/java/com/tutao/practicedemo/test.java
@@ -1,6 +1,7 @@
 package com.tutao.practicedemo;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -10,20 +11,28 @@ import android.view.View;
  */
 
 public class test extends View{
-    
+
+    private Paint mPaint;
+
     public test(Context context) {
         super(context);
     }
 
     public test(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public test(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     public test(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init() {
+        
     }
 }
