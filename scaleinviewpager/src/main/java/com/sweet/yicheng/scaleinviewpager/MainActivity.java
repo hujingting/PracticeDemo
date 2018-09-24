f22ca969cb180ded86c65668fa74abff4b1f54f3
hujingting <15601863591@163.com>
2018-09-24 20:16:50 +0800

    明天继续

--- scaleinviewpager/src/main/java/com/sweet/yicheng/scaleinviewpager/MainActivity.java
+++ scaleinviewpager/src/main/java/com/sweet/yicheng/scaleinviewpager/MainActivity.java
@@ -1,15 +1,44 @@
 package com.sweet.yicheng.scaleinviewpager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * @author jingting
+ */
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.view_pager)
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-//        setContentView();
+
+    }
+
+
+    class MyAdpater extends PagerAdapter {
+
+        
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
     }
 }
