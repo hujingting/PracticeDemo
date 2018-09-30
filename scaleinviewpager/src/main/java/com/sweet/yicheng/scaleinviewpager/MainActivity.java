18a4f6a1ada59dd9ddc411df860250491cc7e04f
hujingting <hujingting@mobase.cn>
2018-09-30 11:42:31 +0800

    Revert "test"
    This reverts commit 05e033ffb5dd4f630010189a25e39d13c6165666.

--- scaleinviewpager/src/main/java/com/sweet/yicheng/scaleinviewpager/MainActivity.java
+++ scaleinviewpager/src/main/java/com/sweet/yicheng/scaleinviewpager/MainActivity.java
@@ -2,8 +2,6 @@ package com.sweet.yicheng.scaleinviewpager;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -16,8 +14,6 @@ import com.tutao.common.utils.ImageUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,23 +23,10 @@ import butterknife.ButterKnife;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private static final int PERIOD_TIME = 3000;
 //    @BindView(R.id.view_pager)
     ViewPager mViewPager;
     int[] bannerRes = new int[] {R.drawable.banner1, R.drawable.banner2, R.drawable.banner3};
     List<View> views;
-
-    private boolean toLoop;
-
-
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            if (msg.what == PERIOD_TIME && toLoop) {
-
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,29 +45,6 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setAdapter(myAdapter);
 
         mViewPager.setCurrentItem(bannerRes.length / 2);
-
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                if (state == ViewPager.SCROLL_STATE_IDLE) {
-                    toLoop = true;
-                } else if (state == ViewPager.SCROLL_STATE_DRAGGING) {
-                    toLoop = false;
-                } else if (state == ViewPager.SCROLL_STATE_SETTLING) {
-
-                }
-            }
-        });
     }
 
     private void initViews() {
@@ -100,10 +60,6 @@ public class MainActivity extends AppCompatActivity {
         CircleImageView rightView = new CircleImageView(this);
 //        middleView.setTag("rightView");
         views.add(rightView);
-
-        BannerTimeTask timeTask = new BannerTimeTask();
-        Timer timer = new Timer();
-        timer.schedule(timeTask, PERIOD_TIME, PERIOD_TIME);
     }
 
 
@@ -139,14 +95,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView((View) object);
-        }
-    }
-
-    class BannerTimeTask extends TimerTask {
-
-        @Override
-        public void run() {
-            handler.sendEmptyMessage(PERIOD_TIME);
         }
     }
 }
